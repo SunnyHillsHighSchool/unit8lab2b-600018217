38427cf5694bb3643af4dac9bddfa9948f650cf2
Johnathan Mitri <>
2021-04-28 00:30:07 +0000

    did it

--- ProfNames.java
+++ ProfNames.java
@@ -2,8 +2,15 @@ public class ProfNames
 {
   public int countNames(String[][]nameArray, String lastName)
   {
-
-
-    return 0;
+    int count = 0;
+    for (String[] row: nameArray)
+    {
+      for (String name: row)
+      {
+        if (name.substring(0, name.indexOf(',')).equals(lastName))
+          count++;
+      }
+    }
+    return count;
   }
 }
